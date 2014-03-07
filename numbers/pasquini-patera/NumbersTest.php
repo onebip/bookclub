aaf40984708f62128ae9a56f9d906a8dfa650a04
Simone Pasquini <simone.pasquini@neomobile.com>
2014-03-07 15:50:55 +0100

    refactoring second test

--- numbers/pasquini-patera/NumbersTest.php
+++ numbers/pasquini-patera/NumbersTest.php
@@ -16,17 +16,27 @@ class NumbersTest extends PHPUnit_Framework_TestCase
         $numberTranslate = new NumberTranslate(99);
         $this->assertEquals($expected,$numberTranslate->numberToLetter());
     }
+
+    public function test300IsTrecento()
+    {
+        $expected = 'trecento';
+        $numberTranslate = new NumberTranslate(300);
+        $this->assertEquals($expected,$numberTranslate->numberToLetter());
+    }
 }
 
 class NumberTranslate
 {
     private $mapSingle = [
+        '0' => '',
         '1' => 'uno',
+        '3' => 'tre',
         '9' => 'nove',
     ];
-    
+    private $cento = 'cento';
     private $mapDouble = [
-        '90' => 'novanta'
+        '90' => 'novanta',
+        '0' => 'cento'
     ];
 
     public function __construct($number)
@@ -39,16 +49,29 @@ class NumberTranslate
         return $this->translate();
     }
 
+    private function giveMeCharAt($position)
+    {
+        return substr($this->number,$position,1);
+    }
+
     private function translate()
     {
-        $numberOfChar = strlen($this->number);
-        if ($numberOfChar == 2) {
-            $key1 = substr($this->number,0,1).'0';
-            $key2 = substr($this->number,1,2);
+        switch(strlen($this->number)) {
+        case 2:
+            $key1 = $this->giveMeCharAt(0).'0';
+            $key2 = $this->giveMeCharAt(1);
             return $this->mapDouble[$key1].$this->mapSingle[$key2];
-        }
-
-        if ($numberOfChar == 1) {
+            break;
+        case 3:
+            $key1 = $this->giveMeCharAt(0);
+            $key2 = $this->giveMeCharAt(1);
+            $key3 = $this->giveMeCharAt(2);
+            $value1 = $this->mapSingle[$key1];
+            $value2 = $this->mapDouble[$key2];
+            $value3 = $this->mapSingle[$key3];
+            return $value1.$value2.$value3;
+            break;
+        default:
             return $this->mapSingle[$this->number];
         }
     }
