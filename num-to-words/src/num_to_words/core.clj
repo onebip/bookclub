473df09a116995aa66dbb2be0fc905936972eeb1
Fabio Pellegrini <fabio.pellegrini@onebip.com>
2016-05-20 16:58:25 +0200

    "Num to words: one digit and two digits numbers tests are green"

--- num-to-words/src/num_to_words/core.clj
+++ num-to-words/src/num_to_words/core.clj
@@ -1,4 +1,74 @@
 (ns num-to-words.core
   (:gen-class))
 
-(defn spell-out)
+
+(def base {0 "zero"
+           1 "one"
+           2 "two"
+           3 "three"
+           4 "four"
+           5 "five"
+           6 "six"
+           7 "seven"
+           8 "eight"
+           9 "nine"})
+
+(def exceptions (merge base {10 "ten" 
+                             11 "eleven"
+                             12 "twelve"
+                             13 "thirteen"
+                             15 "fifteen"
+                             18 "eighteen"
+                             20 "twenty"
+                             30 "thirty"
+                             40 "fourty"
+                             50 "fifty"
+                             60 "sixty"
+                             70 "seventy"
+                             80 "eighty"
+                             90 "ninety"}))
+
+(def big-numbers [1000000 1000 100])
+
+(def position {100 "hundred"
+               1000 "thousand"
+               1000000 "million"})
+
+(defn find-position [number]
+  (loop [queue big-numbers]
+    (let [big-number (first queue)]
+      (if (> (quot number big-number) 0)
+        big-number
+        (recur (rest queue))))))
+
+(defn smart-compose [number]
+  (if (= (mod number 10) 0)
+    (exceptions (* (quot number 10) 10))
+    (str
+      (exceptions (* (quot number 10) 10))
+      "-" 
+      (base (mod number 10)))))
+
+(defn compose [number]
+  (let [pos (find-position number)]
+  (str
+    (base (quot number pos))
+    " "
+    (position pos))))
+
+(defn teen-compose [number]
+  (if (contains? exceptions number)
+    (exceptions number)
+    (str (base (mod number 10)) "teen")))
+
+(defn spell-out [number]
+  (cond 
+    (< number 10) 
+      (base number)
+    (< number 20) 
+      (teen-compose number)
+    (< number 100) 
+      (smart-compose number)
+    :else
+      (compose number)))
+
--- num-to-words/test/num_to_words/core_test.clj
+++ num-to-words/test/num_to_words/core_test.clj
@@ -2,20 +2,40 @@
   (:require [midje.sweet :refer :all]
             [num-to-words.core :refer :all]))
 
-(fact
+(facts
+  (fact "single digit"
     "about number-spelling"
     (spell-out 0) => "zero"
     (spell-out 1) => "one"
-    (spell-out 9) => "nine"
+    (spell-out 9) => "nine")
+
+  (fact "two digits"
+    (spell-out 10) => "ten"
+    (spell-out 11) => "eleven"
+    (spell-out 12) => "twelve"
+    (spell-out 13) => "thirteen"
+    (spell-out 14) => "fourteen"
+    (spell-out 15) => "fifteen"
+    (spell-out 16) => "sixteen"
+    (spell-out 17) => "seventeen"
+    (spell-out 18) => "eighteen"
     (spell-out 19) => "nineteen"
     (spell-out 20) => "twenty"
-    (spell-out 21) => "twenty one"
-    (spell-out 103) => "one hundred and three"
-    (spell-out 999) => "nine hundred and ninety nine"
-    (spell-out 1501) => "one thousand, five hundred and one"
-    (spell-out 9000) => "nine thousand"
-    (spell-out 9999) => "nine thousand, nine hundred and ninety nine"
-    (spell-out 9999999) =>
-    "nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine"
-    (spell-out 9999999999999) =>
-    "nine thousand, nine hundred and ninety nine billion, nine hundred and ninety nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+    (spell-out 21) => "twenty-one"
+    (spell-out 24) => "twenty-four"
+    (spell-out 37) => "thirty-seven"
+    (spell-out 40) => "fourty"
+    (spell-out 54) => "fifty-four"
+    (spell-out 68) => "sixty-eight"
+    (spell-out 99) => "ninety-nine"
+    (spell-out 100) => "one hundred"
+;    (spell-out 103) => "one hundred and three"
+;    (spell-out 999) => "nine hundred and ninety-nine"
+;    (spell-out 1501) => "one thousand, five hundred and one"
+    (spell-out 9000) => "nine thousand")
+;    (spell-out 9999) => "nine thousand, nine hundred and ninetyi-nine"
+;    (spell-out 9999999) =>
+;    "nine million, nine hundred and ninety nine thousand, nine hundred and ninety-nine"
+;    (spell-out 9999999999999) =>
+;    "nine thousand, nine hundred and ninety nine billion, nine hundred and ninety-nine million, nine hundred and ninety nine thousand, nine hundred and ninety nine")
+)
